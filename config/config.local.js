fd21ab59442756ba9edb0af6e56c13d0b9d087af
iovitz <iovitz1999@gmail.com>
2024-04-16 09:52:07 +0800

    chore: 优化数据库配置

--- config/config.local.js
+++ config/config.local.js
@@ -8,13 +8,16 @@ module.exports = () => {
     allowDebugAtProd: true,
   };
 
+  // https://www.sojson.com/encrypt_aes.html
+  const password = "U2FsdGVkX19JY1VzI8bzi3U/fcTzxPPgJB8fTw0kYK4BBCW3VfIeyG1scweJnjd7";
+
   exports.sequelize = {
     dialect: "mysql",
     database: "duuk_book_dev",
     host: "mysql.sqlpub.com",
     port: 3306,
     username: "duuk_book_dev",
-    password: CryptoJS.AES.decrypt("U2FsdGVkX19JY1VzI8bzi3U/fcTzxPPgJB8fTw0kYK4BBCW3VfIeyG1scweJnjd7", "").toString(CryptoJS.enc.Utf8),
+    password: CryptoJS.AES.decrypt(password, "").toString(CryptoJS.enc.Utf8),
   };
 
   return {
--- config/config.prod.js
+++ config/config.prod.js
@@ -3,14 +3,16 @@ const CryptoJS = require("crypto-js");
 module.exports = () => {
   const config = (exports = {});
 
-  // 免费数据库
+  // https://www.sojson.com/encrypt_aes.html
+  const password = "U2FsdGVkX1+SOUbpnfavlUCqPaC66NcYqtt6SGGtRKo0udbZgsInAFXRYVfUqUL3";
+
   exports.sequelize = {
     dialect: "mysql",
     database: "duuk_book_prod",
     host: "mysql.sqlpub.com",
     port: 3306,
     username: "duuk_book_prod",
-    password: CryptoJS.AES.decrypt("U2FsdGVkX1+SOUbpnfavlUCqPaC66NcYqtt6SGGtRKo0udbZgsInAFXRYVfUqUL3", "").toString(CryptoJS.enc.Utf8),
+    password: CryptoJS.AES.decrypt(password, "").toString(CryptoJS.enc.Utf8),
   };
 
   return {
