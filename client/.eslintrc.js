10482e063e76aecdbfc375648984df09993cf1c7
iovitz <iovitz1999@gmail.com>
2024-08-12 14:38:48 +0800

    feat: 优化eslint和typescript开发的CSS提示，以及使用sass

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,32 @@
 module.exports = {
-  root: true,
+  // 为我们提供运行环境，一个环境定义了一组预定义的全局变量
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'eslint:recommended'
-  ],
+  // 一个配置文件可以被基础配置中的已启用的规则继承。
+  extends: ["plugin:react/recommended", "eslint:recommended", "airbnb", "plugin:prettier/recommended"],
+  // 自定义全局变量
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  // 必须有这行才能解析
+  parser: "@typescript-eslint/parser",
+  // 配置解析器支持的语法
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2024,
+    sourceType: "module",
   },
+  plugins: ["react"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  }
-}
+    "no-undef": 0,
+    "react/jsx-filename-extension": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/function-component-definition": 0,
+    "import/extensions": 0,
+    "import/no-unresolved": 0,
+    "react/prop-types": 0
+  },
+};
--- client/src/pages/Tab1.tsx
+++ client/src/pages/Tab1.tsx
@@ -1,6 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import style from "./Tab1.module.scss";
 
 const Tab1: React.FC = () => {
   return (
@@ -16,6 +16,9 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <div className={style.box}>
+          <span className={style.title}>niubi</span>
+        </div>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
