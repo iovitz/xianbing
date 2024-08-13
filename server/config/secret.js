67136a91d51697cb2f500e8ed072e3af8aafcca9
iovitz <iovitz1999@gmail.com>
2024-08-13 11:19:28 +0800

    feat: 优化命名

--- server/config/secret.js
+++ server/config/secret.js
@@ -2,7 +2,7 @@ const { existsSync } = require('fs');
 const { homedir } = require('os');
 const { join } = require('path');
 
-const secretConfigPath = join(homedir(), 'app-config/duuk.json');
+const secretConfigPath = join(homedir(), 'app-config/ollah_server.json');
 
 // 只在生产环境下启用环境JSON
 const config = process.env === 'production' && existsSync(secretConfigPath) ? require(secretConfigPath) : {};
