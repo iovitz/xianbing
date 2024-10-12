2f15deeee4e96501460a56f57e3df125339136ea
iovitz <iovitz1999@gmail.com>
2024-10-12 17:47:37 +0800

    feat: 使用PM2管理项目

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ollah-server',
+      script: 'bootstrap.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
