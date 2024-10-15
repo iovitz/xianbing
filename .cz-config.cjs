0a062dd671323304dd79d0053f2ac6deab0f13a3
iovitz <iovitz1999@gmail.com>
2024-10-15 21:12:15 +0800

    chore(app): 增加部署图标的icon

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -4,47 +4,47 @@ module.exports = {
   types: [
     {
       value: "feat",
-      name: "feat: 完整新功能提交（e.g. 增加用户注册功能）",
+      name: "🎉 feat: 完整新功能提交（e.g. 增加用户注册功能）",
     },
     {
       value: "fix",
-      name: "fix: 修复Bug（e.g. 注册错误码透传失败）",
+      name: "🐛 fix: 修复Bug（e.g. 注册错误码透传失败）",
     },
     {
       value: "update",
-      name: "update: 功能开发阶段性提交（e.g. 设计用户表的ORM）",
+      name: "🚩 update: 功能开发阶段性提交（e.g. 设计用户表的ORM）",
     },
     {
       value: "perf",
-      name: "perf: 对程序性能进行优化（e.g. 加载链路耗时优化）",
+      name: "🚀 perf: 对程序性能进行优化（e.g. 加载链路耗时优化）",
     },
     {
       value: "refactor",
-      name: "refactor: 代码重构，在不影响代码内部行为（e.g. 重构用户注册功能）",
+      name: "💫 refactor: 代码重构，在不影响代码内部行为（e.g. 重构用户注册功能）",
     },
     {
       value: "docs",
-      name: "docs: 只修改了文档相关的文件（e.g. 修改README.md）",
+      name: "📄 docs: 只修改了文档相关的文件（e.g. 修改README.md）",
     },
     {
       value: "style",
-      name: "style: 代码风格、不影响代码功能的更改（e.g. 修改空格缩进，换行规范）",
+      name: "🎨 style: 代码风格、不影响代码功能的更改（e.g. 修改空格缩进，换行规范）",
     },
     {
       value: "ci",
-      name: "ci: CI/CD流程变更（e.g. 减少CI运行耗时）",
+      name: "⌛️ ci: CI/CD流程变更（e.g. 减少CI运行耗时）",
     },
     {
       value: "chore",
-      name: "chore: 构建过程、辅助工具等相关的内容修改（e.g. 更新依赖库）",
+      name: "🥚 chore: 构建过程、辅助工具等相关的内容修改（e.g. 更新依赖库）",
     },
     {
       value: "revert",
-      name: "revert: 恢复上一次提交（e.g. 回滚feat: 增加用户注册功能）",
+      name: "🪜 revert: 恢复上一次提交（e.g. 回滚feat: 增加用户注册功能）",
     },
     {
       value: "test",
-      name: "test: 单元测试相关（e.g. 补充单元测试功能",
+      name: "💣 test: 单元测试相关（e.g. 补充单元测试功能",
     },
   ],
 
