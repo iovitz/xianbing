e15bcf8ef1b4e36529f341d611146e08e6603dda
iovitz <iovitz1999@gmail.com>
2024-10-16 19:40:05 +0800

    chore: 优化部署脚本

--- server/src/config/config.prod.ts
+++ server/src/config/config.prod.ts
@@ -1,32 +1,3 @@
 import { MidwayConfig } from '@midwayjs/core';
 
-const env = process.env;
-export default {
-  // use for cookie sign key, should change to your own and keep security
-  keys: env.XIANBING_APP_COOKIE_KEY,
-  koa: {
-    port: Number(env.XIANBING_APP_PORT),
-  },
-  sequelize: {
-    dataSource: {
-      // 第一个数据源，数据源的名字可以完全自定义
-      default: {
-        database: env.XIANBING_DB_MYSQL_DB_NAME,
-        username: env.XIANBING_DB_MYSQL_USER,
-        password: env.XIANBING_DB_MYSQL_PASSWORD,
-        host: env.XIANBING_DB_MYSQL_HOST,
-        port: Number(env.XIANBING_DB_MYSQL_PORT),
-        encrypt: false,
-        dialect: 'mysql',
-        define: { charset: 'utf8' },
-        timezone: '+08:00',
-        entities: ['mysql'],
-        sync: true,
-        repositoryMode: true,
-      },
-    },
-  },
-  multiAvatar: {
-    key: env.XIANBING_MULTIAVATAR_KEY,
-  },
-} as MidwayConfig;
+export default {} as MidwayConfig;
