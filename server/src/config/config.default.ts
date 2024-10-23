086b94d93e891cf8fd48fe3bcf45e27688868120
iovitz <iovitz1999@gmail.com>
2024-10-23 12:16:32 +0800

    chore: 修复sqlite3无法启动的问题

--- server/src/config/config.default.ts
+++ server/src/config/config.default.ts
@@ -30,9 +30,12 @@ export default {
         type: 'better-sqlite3', // 使用 better-sqlite3 驱动
         // 数据放在 ~/sqlite 目录下
         database: join(homedir(), 'sqlite', 'xianbing.sqlite'),
-        // 使用better-sqlite3一定要选上这个
-        nativeBinding:
-          'node_modules/better-sqlite3/build/Release/better_sqlite3',
+        // 使用 better-sqlite3 一定要选上这个，否则会出现报错：Error: Could not locate the bindings file.
+        // 或者每次安装完依赖去 better-sqlite3 目录下执行 npm run build-release 一下，也能解决问题
+        // 错误原因查看官网的nativeBinding选项介绍
+        // 官网：https://github.com/WiseLibs/better-sqlite3/blob/master/docs/api.md
+        // typeorm 对 better_sqlite3 的支持度不太好，没有直接build
+        nativeBinding: 'src/shared/sqlite/better_sqlite3.node', // 直接指定已经打包编译的文件
 
         synchronize: true, // 如果第一次使用，不存在表，有同步的需求可以写 true，注意会丢数据
         logging: false,
