f5e88bf6f6fae9a1fe183b55c0997d5354aad9db
iovitz <iovitz1999@gmail.com>
2024-10-23 14:37:57 +0800

    feat: 增加traceID编号

--- server/src/middleware/tracer.middleware.ts
+++ server/src/middleware/tracer.middleware.ts
@@ -13,10 +13,13 @@ export class TracerMiddleware implements IMiddleware<Context, NextFunction> {
     10
   );
 
+  private count = 1;
+
   resolve() {
     return async (ctx: Context, next: NextFunction) => {
       const stime = process.hrtime.bigint();
-      ctx.traceId = this.tracerIdGenerator();
+      ctx.traceId =
+        this.tracerIdGenerator() + `${this.count++}`.padStart(10, '0');
       ctx.set('x-xb-trace-id', ctx.traceId);
       ctx.logger.info(
         `+++${ctx.userId ?? '??'} ${ctx.method} ${ctx.url}`,
